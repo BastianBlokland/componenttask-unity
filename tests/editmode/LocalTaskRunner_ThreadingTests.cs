34267c8aebf2be53d2c9584cd6a1660507952a81
Bastian Blokland <bastianblokland@gmail.com>
2019-07-24 20:52:32 +0300

    Add 'ExecuteThreadingStressTest' test

--- tests/editmode/LocalTaskRunner_ThreadingTests.cs
+++ tests/editmode/LocalTaskRunner_ThreadingTests.cs
@@ -21,14 +21,14 @@ namespace ComponentTask.Tests.EditMode
             using (var runner = new LocalTaskRunner(exHandler))
             {
                 // Start the stress tasks.
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < 50; i++)
                     runner.StartTask(StressAsync);
 
                 // Execute enough times to finish all tasks.
-                for (int i = 0; i < 999; i++)
+                for (int i = 0; i < 99; i++)
                 {
                     runner.Execute();
-                    Thread.Yield();
+                    Thread.Sleep(10);
                 }
 
                 // Assert that all tasks have finished.
@@ -38,8 +38,38 @@ namespace ComponentTask.Tests.EditMode
             async Task StressAsync()
             {
                 /* This stresses the sync-context by posting callbacks from many different threads. */
-                for (int i = 0; i < 100; i++)
+                for (int i = 0; i < 50; i++)
                     await Task.Run(() => Thread.Yield());
+            }
+        }
+
+        [Test]
+        public void ExecuteThreadingStressTest()
+        {
+            var exHandler = new MockExceptionHandler();
+            using (var runner = new LocalTaskRunner(exHandler))
+            {
+                // Start many tasks tasks.
+                for (int i = 0; i < 100; i++)
+                    runner.StartTask(TestAsync);
+
+                for (int interation = 0; interation < 11; interation++)
+                {
+                    // Execute from many different threads in parallel.
+                    for (int thread = 0; thread < 100; thread++)
+                        ThreadPool.QueueUserWorkItem(r => ((LocalTaskRunner)r).Execute(), runner);
+
+                    Thread.Sleep(millisecondsTimeout: 100);
+                }
+
+                // Assert that all tasks have finished.
+                runner.AssertRunningTaskCount(0);
+            }
+
+            async Task TestAsync()
+            {
+                for (int i = 0; i < 10; i++)
+                    await Task.Yield();
             }
         }
     }
